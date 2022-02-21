69b57347198be977090f1fe4006babbdc0c679d6
Adamsiksik <78469824+Adamsiksik@users.noreply.github.com>
2022-02-21 20:05:42 +0200

    gender

--- login_bloc/lib/src/screens/info.dart
+++ login_bloc/lib/src/screens/info.dart
@@ -17,6 +17,8 @@ class _InfoScreenState extends State<InfoScreen> {
   GlobalKey<FormState> formkey = GlobalKey<FormState>();
   final myController = TextEditingController();
   DateTime _dateTime = DateTime.now();
+    int? groupValue;
+
 
   @override
   void dispose() {
@@ -28,88 +30,144 @@ class _InfoScreenState extends State<InfoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(title: Text("Personal Infromation")),
-        body: SingleChildScrollView(
-            child: Form(
-                autovalidateMode:
-                    AutovalidateMode.always, //check for validation while typing
-                key: formkey,
-                child: Column(children: <Widget>[
-                  Padding(
-                      padding: const EdgeInsets.only(top: 60.0),
-                      child: Center(
-                          child: SizedBox(
-                              width: 340,
-                              height: 80,
-                              child: GradientText(
-                                'Please Enter Your Data',
-                                style: const TextStyle(fontSize:30),
-                                gradient: LinearGradient(colors: [
-                                  Colors.black,
-                                  Colors.red,
-                                ]),
-                              )))),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                    child: TextFormField(
-                      controller: myController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Username',
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please Fill Up This Field';
-                        }
-                      },
-                    ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Text("Personal Infromation")),
+      body: SingleChildScrollView(
+        child: Form(
+          autovalidateMode:
+              AutovalidateMode.always, //check for validation while typing
+          key: formkey,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: Center(
+                      child: SizedBox(
+                          width: 340,
+                          height: 50,
+                          child: GradientText(
+                            'Please Enter Your Data',
+                            style: const TextStyle(fontSize: 30),
+                            gradient: LinearGradient(colors: [
+                              Colors.black,
+                              Colors.red,
+                            ]),
+                          )))),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                child: TextFormField(
+                  controller: myController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
                   ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 22, vertical: 0),
-                    child: Row(children: <Widget>[
-                      GradientText(
-                        'Date Of Birth:',
-                        style: const TextStyle(fontSize: 20),
-                        gradient: LinearGradient(colors: [
-                          Colors.black,
-                          Colors.blueGrey,
-                        ]),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.calendar_today),
-                        padding: EdgeInsets.only(left: 5),
-                        onPressed: () {
-                          showDatePicker(
-                                  context: context,
-                                  initialDate: _dateTime == null
-                                      ? DateTime.now()
-                                      : _dateTime,
-                                  firstDate: DateTime(1950),
-                                  lastDate: DateTime.now())
-                              .then((date) {
-                            setState(() {
-                              _dateTime = date!;
-                            });
-                          });
-                        },
-                      ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Fill Up This Field';
+                    }
+                  },
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 22, vertical: 0),
+                child: Row(children: <Widget>[
+                  GradientText(
+                    'Date Of Birth:',
+                    style: const TextStyle(fontSize: 20),
+                    gradient: LinearGradient(colors: [
+                      Colors.black,
+                      Colors.blueGrey,
                     ]),
                   ),
-                 Container(
-                    padding: EdgeInsets.symmetric(horizontal: 22, vertical: 20),
-                    child: Row(children: const <Widget>[
-                      GradientText(
-                        'Gender:',
-                        style: TextStyle(fontSize: 20),
-                        gradient: LinearGradient(colors: [
-                          Colors.black,
-                          Colors.blueGrey,
-                        ]),
+                  IconButton(
+                    icon: Icon(Icons.calendar_today),
+                    padding: EdgeInsets.only(left: 5),
+                    onPressed: () {
+                      print(groupValue.toString());
+                      showDatePicker(
+                              context: context,
+                              initialDate: _dateTime == null
+                                  ? DateTime.now()
+                                  : _dateTime,
+                              firstDate: DateTime(1950),
+                              lastDate: DateTime.now())
+                          .then((date) {
+                        setState(() {
+                          _dateTime = date!;
+                        });
+                      });
+                    },
+                  ),
+                ]),
+              ),
+              Container(
+               padding: EdgeInsets.symmetric(horizontal:25, vertical: 10),
+              child:Column(
+                children: [
+                  Row(
+                    
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Expanded(
+                        child:GradientText(
+                    'Gender:',
+                    style: const TextStyle(fontSize: 20),
+                    gradient: LinearGradient(colors: [
+                      Colors.black,
+                      Colors.blueGrey,
+                    ]),
+                  ),
+                  flex:1,
+                      ),
+                    Expanded(
+                        child: Row(
+                          children: [
+                            Radio<int>(
+                  value: 1,
+                  groupValue: groupValue,
+                
+                  toggleable: true,
+                  onChanged: (int? value) {
+                    setState(() {
+                      groupValue = value;
+                    });
+                  }),
+                            Expanded(
+                              child: Text('Male'),
+                            )
+                          ],
+                        ),
+                        flex: 1,
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                              Radio<int>(
+                  value: 2,
+                  groupValue: groupValue,
+                
+                  toggleable: true,
+                  onChanged: (int? value) {
+                    setState(() {
+                      groupValue = value;
+                    });
+                  }),
+                            Expanded(child: Text('Female'))
+                          ],
+                        ),
+                        flex: 1,
                       ),
                      
-                    ]),
+                    ],
                   ),
-                ]))));
+                ],
+              ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      
+    );
   }
 }
