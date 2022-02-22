6182370020edd8255c7c9997dcd048b04807b785
Adamsiksik <78469824+Adamsiksik@users.noreply.github.com>
2022-02-22 20:02:49 +0200

    Update app.dart

--- login_bloc/lib/src/app.dart
+++ login_bloc/lib/src/app.dart
@@ -6,27 +6,20 @@ import 'screens/login.dart';
 import 'screens/first.dart';
 import 'screens/info.dart';
 
-
 class App extends StatelessWidget {
   @override
   build(context) {
     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
-  routes: {
-  
-    '/home': (context) => First(),
-     '/login': (context) => LoginScreen(),
-          '/signup': (context) => SignupScreen(),
-
-      '/info': (context) => InfoScreen(),
-
-
-  },
+      routes: {
+        '/home': (context) => First(),
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignupScreen(),
+        '/info': (context) => InfoScreen(),
+      },
       title: 'Log in screen',
       home: Scaffold(
-
         body: Center(child: First()),
       ),
     );
